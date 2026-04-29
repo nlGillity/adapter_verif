3786e611b460edbf74cc7dc8876c7a29c35f4080
Danil Zavarnitcyn <danilzav@bk.ru>
2026-04-29 22:14:03 +0000

    Added Enviroment

---
+++ src/adapter/env/env.sv
@@ -0,0 +1,41 @@
+class env;
+
+    master          master_obj;
+    slave           slave_obj;
+    scoreboard      scoreboard_obj;
+    reference_model reference_obj;
+
+    //=============================================================================
+    // Functions
+    //=============================================================================
+
+    function new();
+        master_obj     = new();
+        slave_obj      = new();
+        scoreboard_obj = new();
+        reference_obj  = new();
+    endfunction
+
+    //-----------------------------------------------------------------------------
+
+    virtual function void configure(test_config cfg);
+        master_obj    .configure(cfg);
+        slave_obj     .configure(cfg);
+        scoreboard_obj.configure(cfg);
+        reference_obj .configure(cfg);
+    endfunction 
+
+    //=============================================================================
+    // Tasks
+    //=============================================================================
+    virtual task run();
+        fork
+            master_obj    .run();
+            slave_obj     .run();
+            reference_obj .run();
+            scoreboard_obj.run();
+        join_any
+        disable fork;
+    endtask
+
+endclass
